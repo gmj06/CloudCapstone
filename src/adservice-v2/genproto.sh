a9b222ffcaf8b174494933b79a16ae1ce1533b4d
gmj06 <gowrimj2006@gmail.com>
2021-12-10 21:06:57 -0800

    Step 6  implemented

--- src/adservice-v2/genproto.sh
+++ src/adservice-v2/genproto.sh
@@ -3,3 +3,7 @@
 set -e
 
 # TODO: Add the commands to generate the gRPC files
+PATH=$PATH:$HOME/go/bin
+PROTODIR=../../pb
+
+python3 -m  grpc_tools.protoc -I $PROTODIR --python_out=./ --grpc_python_out=./ $PROTODIR/demo.proto
--- src/frontend/genproto.sh
+++ src/frontend/genproto.sh
@@ -16,7 +16,7 @@
 
 #!/bin/bash -e
 
-PATH=$PATH:$GOPATH/bin
+PATH=$PATH:$HOME/go/bin
 protodir=../../pb
 
 # protoc --go_out=plugins=grpc:genproto -I $protodir $protodir/demo.proto
